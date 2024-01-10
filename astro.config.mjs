5ab31da54792d090dee7d0e3815fe5bf4fd493f4
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-01-10 15:31:59 -0600

    add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wavelandweb.com',
   output: 'hybrid',
   scopedStyleStrategy: 'class',
-  integrations: [
-    sitemap({
-      lastmod: new Date(),
-    }),
-    mdx(),
-  ],
+  integrations: [sitemap({
+    lastmod: new Date()
+  }), mdx()],
   markdown: {
     shikiConfig: {
-      theme: 'material-theme-palenight',
-    },
+      theme: 'material-theme-palenight'
+    }
   },
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
-})
+  adapter: netlify()
+});
